27cbb63641705f6cbbb31f248acd6e2cd398cfee
SadidNj <mushfiquemortuza.sadid@gmail.com>
2026-07-25 13:57:14 +0600

    Add Models

---
+++ Models/BloodBankDbContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace blood.Models;
+
+public partial class BloodBankDbContext : DbContext
+{
+    public BloodBankDbContext()
+    {
+    }
+
+    public BloodBankDbContext(DbContextOptions<BloodBankDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Donation> Donations { get; set; }
+
+    public virtual DbSet<Donor> Donors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Donation>(entity =>
+        {
+            entity.HasKey(e => e.DonationId).HasName("PK__Donation__C5082EFB0C43A699");
+
+            entity.ToTable("Donation");
+
+            entity.Property(e => e.CampName).HasMaxLength(100);
+
+            entity.HasOne(d => d.Donor).WithMany(p => p.Donations)
+                .HasForeignKey(d => d.DonorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Donation__DonorI__25869641");
+        });
+
+        modelBuilder.Entity<Donor>(entity =>
+        {
+            entity.HasKey(e => e.DonorId).HasName("PK__Donor__052E3F78A6871FA2");
+
+            entity.ToTable("Donor");
+
+            entity.Property(e => e.BloodGroup).HasMaxLength(10);
+            entity.Property(e => e.City).HasMaxLength(50);
+            entity.Property(e => e.ContactNo).HasMaxLength(20);
+            entity.Property(e => e.FullName).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
